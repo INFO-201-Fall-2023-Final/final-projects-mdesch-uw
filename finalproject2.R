059d26131b67e85df9969e9cc3278a3fbff973cd
mdesch-uw <151972034+mdesch-uw@users.noreply.github.com>
2023-11-26 16:31:23 -0800

    Add files via upload
    This is the booksheet that is a combination of two data frames I converted into .csv format, and a file that runs a summary of it.

---
+++ finalproject2.R
@@ -0,0 +1,13 @@
+#Max Deschenes
+#mdesch@uw.edu
+#I worked together with my group member, Irene Wang
+
+#all library statements must be at the top of the page!
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+df <- read.csv("booksheet.csv")
+
+summary(df)
+
