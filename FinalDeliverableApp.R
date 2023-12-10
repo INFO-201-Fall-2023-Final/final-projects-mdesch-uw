bf09cb4e09d4d0c013a7e7b5c9b278a30f61bc92
mdesch-uw <151972034+mdesch-uw@users.noreply.github.com>
2023-12-09 22:27:39 -0800

    UPDATING FILES - these are the most recent changes
    More progress done on the project!

--- FinalDeliverableApp.R
+++ FinalDeliverableApp.R
@@ -53,7 +53,7 @@ page_two <- tabPanel(
   "Second Page", # label for the tab in the navbar
   titlePanel("Page 2"), # show with a displayed title
   sidebarLayout(
-    sidebarPanel(plotlyOutput(outputId = "column")
+    sidebarPanel(plotlyOutput(outputId = "column"),
     ),
     mainPanel(
       h3("Food Insecurity by Demographics"),
@@ -69,9 +69,13 @@ page_two <- tabPanel(
       ("To the left, a bar plot showcases the food insecurity rates of students by ethnicity.
        This difference is important to be aware of with these kinds of issues; it's clear that
        while all students are impacted by these effects, certain communities are far more 
-       affected. This is also noteworthy because college is already more exclusive to
-       nonwhite students, so food and housing insecurity rates are another way that college
-       can be limited in diversity."),
+       affected. Food and housing insecurity rates are a way that colleges can be more exclusive
+       towards non-white students; there are a variety of socie-economic reasons that certain groups are more
+       vulnerable to food insecurity. For example, food deserts are areas where nutritious and
+       affordable food are difficult to find. These typically are low-income communities, and/or 
+       predominantly non-white communities. Combining issues like these with the already existing
+       inequalities in terms of attending college for marginalized communities, it is clear that
+       while all students are affected, some are significantly more affected than others and "),
     )
   )
 
@@ -82,21 +86,51 @@ page_two <- tabPanel(
 page_three <- tabPanel(
   "Third Page", # label for the tab in the navbar
   titlePanel("Page 3"), # show with a displayed title
-  h3("COVID-19's effect on students"),
-  p("Research shows that half of all college students across the country are 
+  sidebarLayout(
+    sidebarPanel(plotlyOutput(outputId = "line"),
+    ),
+    mainPanel(
+    h3("COVID-19's effect on students"),
+    p("Research shows that half of all college students across the country are 
         likely to experience some level of food or housing insecurity throughout 
         the duration of their education. Additionally, it has been observed that 
         COVID-19 exacerbated issues of food insecurity and housing insecurity for 
-        all demographic groups, but that youths aged 18-24, the typical age of 
-        college students, were most vulnerable in these cases due to campus shutdowns 
-        depriving students of housing and food sources, and the likelihood of 
-        unemployment or loss of jobs during this period."),
+        all demographic groups, but that younger people (represented here by the 
+        dark red line on the chart), the typical age of college students, were most 
+        vulnerable in these cases due to campus shutdowns depriving students of housing 
+        and food sources, and the likelihood of unemployment or loss of jobs during this 
+        period. Comparatively, these percentages decrease at older ages."),
+    br(),
+    p("A study performed by Oregon State University observes that the percentage of college
+      aged students reporting food insecurity in the state tripled from 4.50% to 13.5%.
+      This shows clearly that COVID-19 had an extremely significant effect on student food
+      insecurity. While other age groups did show an increase in food insecurity rates, 
+      these were less drastic (both roughly doubling).")
+    )
+  )
 )
 
 # Define content for the fourth page
 page_four <- tabPanel(
   "Fourth Page", # label for the tab in the navbar
   titlePanel("Page 4"), # show with a displayed title
+  h3("Solutions and Predictions"),
+  p("Students struggling financially is not a new phenomenon, but it has been 
+  exacerbated by the COVID-19 pandemic. Campus shutdowns have cut off many 
+  students from their planned housing and food resources, as well as from 
+  resources such as food pantries or campus work opportunities to offset the 
+  cost of food. Similarly, most college students do not have a very secure or 
+  lucrative job, and they are in the demographic that is most likely to lose 
+  their jobs during the pandemic. Much like the overall issue, these issues are 
+  even worse for vulnerable communities, which are hit harder and take longer to 
+  recover from situations such as these. "),
+  br(),
+  p("These issues could be fixed; cheaper tuition, more financial aid and support 
+  for students, more resources such as food banks or free meals or housing, and 
+  government student debt relief would vastly alleviate these stressors for so 
+  many students. But until there is a strong economic incentive for these 
+  institutions to do so, such as a sharp downtick in student enrollment due to 
+  prohibitive cost, the prices seem set to continue to rise higher.")
   # ...more content would go here...
 )
 
@@ -111,9 +145,18 @@ ui <- navbarPage(
 )
 server <- function(input, output, session) {
   output$column <- renderPlotly({ 
-    p <- ggplot(data, aes(x=Ethnicity, y=Food_Insecurity_Rate)) + 
-      geom_bar(stat = "identity")
+    p <- ggplot(data, aes(x=Ethnicity, y=Food_Insecurity_Rate, fill = Ethnicity)) + 
+      geom_bar(stat = "identity") 
     })
+  output$line <- renderPlotly({
+    o <- ggplot(data2, aes(x=Year)) + 
+      geom_line(aes(y = Percentage.Insecurity), color = "darkred") + 
+      geom_line(aes(y = Age46.64), color="steelblue") +
+      geom_line(aes(y = Age65), color="darkgreen") +
+      labs(x = "Year",
+           y = "Food Insecurity (%)",
+           color = "Legend") 
+  })
 }
 
 
--- FinalDeliverableCode.R
+++ FinalDeliverableCode.R
@@ -11,8 +11,27 @@ df <- filter(ethnicity_selected, Type == "Food Insecurity")
 # creating a data frame for the GG plot
 data <- data.frame(
   Ethnicity=c("American Indian", "Asian American", "Black/African American", "Hispanic/Latino", "Pacific Islander", "Multiracial", "White") ,  
-  Food_Insecurity_Rate=c(52.2, 29.7, 56.0, 46.5, 48.1, 44.7, 34.8)
+  Food_Insecurity_Rate=c(52.2, 29.7, 56.0, 46.5, 48.1, 44.7, 34.8),
+  Housing_Insecurity_Rate=c(59.6, 26.7, 48.0, 38.8, 33.3, 35.5, 32.7),
+  Homelessness_Rate=c(17.0, 7.6, 19.9, 10.7, 15.0, 14.0, 10.4)
 )
+
+data2 <- data.frame(
+  Year=c(2019, 2021),
+  Percentage.Insecurity=c(4.50, 13.50),
+  Age46.64=c(4.20, 9.20),
+  Age65=c(2.70, 6.90)
+)
+#p <- ggplot(data2, aes(x=Year)) + 
+  #geom_line(aes(y = Percentage.Insecurity), color = "darkred") + 
+  #geom_line(aes(y = Age46.64), color="steelblue") +
+  #geom_line(aes(y = Age65), color="darkgreen") +
+  #labs(x = "Year",
+       #y = "Food Insecurity (%)",
+       #color = "Legend") 
+
+#print(p)
+
 # just testing GG plot below, this code has been commented out now
 #p <- ggplot(data, aes(x=Ethnicity, y=Food_Insecurity_Rate)) + 
   #geom_bar(stat = "identity")
